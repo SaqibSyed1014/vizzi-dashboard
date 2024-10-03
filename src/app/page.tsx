c2b83e8e81ac0d80aad4712d14507a97b527a7b3
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-10-03 15:57:23 +0500

    loader added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import DashboardLayout from "@/layout/dashboard-layout";
 import Dashboard from "@/app/dashboard/page";
+import {Suspense} from "react";
+import Loading from "@/app/loading";
 
 export default function Home() {
   return (
       <DashboardLayout>
-        <Dashboard />
+          <Suspense fallback={<Loading />}>
+              <Dashboard />
+          </Suspense>
       </DashboardLayout>
   );
 }
