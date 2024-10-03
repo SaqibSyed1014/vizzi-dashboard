7dbfe6324ab0a71649863ab6d267b2ef1304beb7
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-10-03 13:10:10 +0500

    removed font weight import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800"],
+  weight: [],
 });
 
 export const metadata: Metadata = {
